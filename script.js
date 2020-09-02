34d83deaa781771ff9159d4ab6868ad07092a7f1
zagPatryk <zag.patryk@gmail.com>
2020-09-02 20:03:59 +0200

    GithubIo addon fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    const apiRoot = 'https://blooming-savannah-27502.herokuapp.com/v1/task';
-    const trelloApiRoot = 'https://blooming-savannah-27502.herokuapp.com/v1/trello';
+    const apiRoot = 'https://blooming-savannah-27502.herokuapp.com/v1/task/';
+    const trelloApiRoot = 'https://blooming-savannah-27502.herokuapp.com/v1/trello/';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
 
